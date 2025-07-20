2fb733c7c492826898d28be5b685ee5636aef3a7
woodie2wopper <woodie2wopper@gmail.com>
2025-07-20 17:19:37 +0900

    feat: task3.1 トップページと記事一覧ページを実装

---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const posts = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional(),
+		category: z.string().optional(),
+		isDraft: z.boolean().optional(),
+	}),
+});
+
+export const collections = { blog, posts }; 
